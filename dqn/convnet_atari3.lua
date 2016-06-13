491bd7567ebacb1ad4c668c5ecfd5b6c063cd6bb
Nan Yang <yangnan719@gmail.com>
2016-06-13 11:27:59 +0200

    adjust convlayer parameters.

--- dqn/convnet_atari3.lua
+++ dqn/convnet_atari3.lua
@@ -6,8 +6,8 @@ See LICENSE file for full terms of limited license.
 require 'convnet'
 
 return function(args)
-    args.n_units        = {8, 16}
-    args.filter_size    = {3, 2}
+    args.n_units        = {16,32}
+    args.filter_size    = {3, 3}
     args.filter_stride  = {1, 1}
     args.n_hid          = {64}
     args.nl             = nn.Rectifier
